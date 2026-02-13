33d2fe3ef630be2ab3adfab342c0c78515ce0d0d
susanact99 <sctrabaj@gmail.com>
2026-02-13 20:44:59 +0100

    Update logic in contact form

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -19,9 +19,9 @@ export default function ContactForm() {
     e.preventDefault()
     setIsSubmitting(true)
 
-    const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID!
-    const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID!
-    const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY!
+    const SERVICE_ID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID || 'service_eqoq632';
+  const TEMPLATE_ID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID || 'template_4uxl0y5';
+  const PUBLIC_KEY = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || '-GzGEHQC_Ao5I8ijX';
 
     if (!formRef.current) return
 
